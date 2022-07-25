5bc0d1410e451d12ec9cf24d363cf83bf3fca801
Joël Rimaz <jo.ante2011@gmail.com>
2022-07-25 14:56:55 +0200

    More sprites and testing
    Problems with hover + mouseout.
    
    Need to look into scenes

--- JS/main.js
+++ JS/main.js
@@ -2,149 +2,272 @@ kaboom({
     width: 124,
     height: 84,
     clearColor: [0, 0, 0],
-    scale: 8
+    scale: 8,
+    font: "sink"
 });
 
 loadSpriteAtlas("Images/Sprite_sheet.png", {
-	"pub_train": {
-		"x": 0,
-		"y": 0,
-		"width": 1488,
-		"height": 504,
-		"sliceX": 12,
-		"sliceY":12,
-		"anims": {
-			"animated_BG": {
-				"from": 0,
-				"to": 142,
-				"speed": 10,
-				"loop": true
-			},
-			"idle": 0
-		}
+    "title": {
+        "x": 1488,
+        "y": 0,
+        "width": 124,
+        "height": 588,
+        "sliceX": 1,
+        "sliceY": 14,
+        "anims": {
+            "animated_BG": {
+                "from": 0,
+                "to": 13,
+                "speed": 10,
+                "loop": true
+            },
+            "idle": 0
+        }
     },
-    "pub_marche": {
-		"x": 0,
-		"y": 504,
-		"width": 1488,
-		"height": 42,
-		"sliceX": 12,
-		"anims": {
-			"animated_BG": {
-				"from": 0,
-				"to": 11,
-				"speed": 10,
-				"loop": true
-			},
-			"idle": 0
-		}
+    "event_train": {
+        "x": 0,
+        "y": 0,
+        "width": 1488,
+        "height": 504,
+        "sliceX": 12,
+        "sliceY": 12,
+        "anims": {
+            "animated_BG": {
+                "from": 0,
+                "to": 142,
+                "speed": 10,
+                "loop": true
+            },
+            "idle": 0
+        }
     },
-    "argent_ag": {
-		"x": 0,
-		"y": 546,
-		"width": 1488,
-		"height": 42,
-		"sliceX": 12,
-		"anims": {
-			"animated_BG": {
-				"from": 0,
-				"to": 10,
-				"speed": 10,
-				"loop": true
-			},
-			"idle": 0
-		}
+    "event_marche": {
+        "x": 0,
+        "y": 504,
+        "width": 1488,
+        "height": 42,
+        "sliceX": 12,
+        "anims": {
+            "animated_BG": {
+                "from": 0,
+                "to": 11,
+                "speed": 10,
+                "loop": true
+            },
+            "idle": 0
+        }
     },
-    "pub_journal": {
-		"x": 0,
-		"y": 588,
-		"width": 1364,
-		"height": 42,
-		"sliceX": 11,
-		"anims": {
-			"animated_BG": {
-				"from": 0,
-				"to": 11,
-				"speed": 10,
-				"loop": true
-			},
-			"idle": 0
-		}
+    "event_ag": {
+        "x": 0,
+        "y": 546,
+        "width": 1488,
+        "height": 42,
+        "sliceX": 12,
+        "anims": {
+            "animated_BG": {
+                "from": 0,
+                "to": 10,
+                "speed": 10,
+                "loop": true
+            },
+            "idle": 0
+        }
     },
-    "argent_poste": {
-		"x": 0,
-		"y": 630,
-		"width": 1488,
-		"height": 42,
-		"sliceX": 12,
-		"anims": {
-			"animated_BG": {
-				"from": 0,
-				"to": 11,
-				"speed": 10,
-				"loop": true
-			},
-			"idle": 0
-		}
+    "event_journal": {
+        "x": 0,
+        "y": 588,
+        "width": 1364,
+        "height": 42,
+        "sliceX": 11,
+        "anims": {
+            "animated_BG": {
+                "from": 0,
+                "to": 11,
+                "speed": 10,
+                "loop": true
+            },
+            "idle": 0
+        }
+    },
+    "event_poste": {
+        "x": 0,
+        "y": 630,
+        "width": 1488,
+        "height": 42,
+        "sliceX": 12,
+        "anims": {
+            "animated_BG": {
+                "from": 0,
+                "to": 11,
+                "speed": 10,
+                "loop": true
+            },
+            "idle": 0
+        }
     },
     "event_coupole": {
-		"x": 0,
-		"y": 672,
-		"width": 992,
-		"height": 42,
-		"sliceX": 8,
-		"anims": {
-			"animated_BG": {
-				"from": 0,
-				"to": 7,
-				"speed": 10,
-				"loop": true
-			},
-			"idle": 0
-		}
+        "x": 0,
+        "y": 672,
+        "width": 992,
+        "height": 42,
+        "sliceX": 8,
+        "anims": {
+            "animated_BG": {
+                "from": 0,
+                "to": 7,
+                "speed": 10,
+                "loop": true
+            },
+            "idle": 0
+        }
     },
-    "pub_affiche": {
-		"x": 0,
-		"y": 714,
-		"width": 1612,
-		"height": 42,
-		"sliceX": 13,
-		"anims": {
-			"animated_BG": {
-				"from": 0,
-				"to": 12,
-				"speed": 10,
-				"loop": true
-			},
-			"idle": 0
-		}
+    "event_affiche": {
+        "x": 0,
+        "y": 714,
+        "width": 1612,
+        "height": 42,
+        "sliceX": 13,
+        "anims": {
+            "animated_BG": {
+                "from": 0,
+                "to": 12,
+                "speed": 10,
+                "loop": true
+            },
+            "idle": 0
+        }
     },
-    "pub_radio": {
-		"x": 0,
-		"y": 756,
-		"width": 1612,
-		"height": 42,
-		"sliceX": 13,
-		"anims": {
-			"animated_BG": {
-				"from": 0,
-				"to": 12,
-				"speed": 10,
-				"loop": true
-			},
-			"idle": 0
-		}
+    "event_radio": {
+        "x": 0,
+        "y": 756,
+        "width": 1612,
+        "height": 42,
+        "sliceX": 13,
+        "anims": {
+            "animated_BG": {
+                "from": 0,
+                "to": 12,
+                "speed": 10,
+                "loop": true
+            },
+            "idle": 0
+        }
     },
-})
+    "score_train": {
+        "x": 1612,
+        "y": 462,
+        "width": 124,
+        "height": 42,
+        "sliceX": 1,
+        "sliceY": 1,
+        "anims": {
+            "idle": 0
+        }
+    },
+    "score_marche": {
+        "x": 1612,
+        "y": 504,
+        "width": 124,
+        "height": 42,
+        "sliceX": 1,
+        "sliceY": 1,
+        "anims": {
+            "idle": 0
+        }
+    },
+    "score_ag": {
+        "x": 1612,
+        "y": 546,
+        "width": 124,
+        "height": 42,
+        "sliceX": 1,
+        "sliceY": 1,
+        "anims": {
+            "idle": 0
+        }
+    },
+    "score_journal": {
+        "x": 1612,
+        "y": 588,
+        "width": 124,
+        "height": 42,
+        "sliceX": 1,
+        "sliceY": 1,
+        "anims": {
+            "idle": 0
+        }
+    },
+    "score_poste": {
+        "x": 1612,
+        "y": 630,
+        "width": 124,
+        "height": 42,
+        "sliceX": 1,
+        "sliceY": 1,
+        "anims": {
+            "idle": 0
+        }
+    },
+    "score_coupole": {
+        "x": 1612,
+        "y": 672,
+        "width": 124,
+        "height": 42,
+        "sliceX": 1,
+        "sliceY": 1,
+        "anims": {
+            "idle": 0
+        }
+    },
+    "score_affiche": {
+        "x": 1612,
+        "y": 714,
+        "width": 124,
+        "height": 42,
+        "sliceX": 1,
+        "sliceY": 1,
+        "anims": {
+            "idle": 0
+        }
+    },
+    "score_radio": {
+        "x": 1612,
+        "y": 756,
+        "width": 124,
+        "height": 42,
+        "sliceX": 1,
+        "sliceY": 1,
+        "anims": {
+            "idle": 0
+        }
+    },
+});
 
 const elem1 = add([
-	pos(0,0),
-	sprite("pub_radio", { anim: "animated_BG" }),
+    pos(0, 0),
+    sprite("event_radio", { anim: "animated_BG" }),
+    area(),
+    "radio_event",
+    "radio"
 ])
 
 const elem2 = add([
-	pos(0,42),
-	sprite("pub_marche", { anim: "animated_BG" }),
+    pos(0, 42),
+    sprite("event_marche", { anim: "animated_BG" }),
+    area(),
+    "marche_event",
+    "marche"
 ])
 
-elem2.flipY(true); elem2.flipX(true)
+let mesObj = get();
+
+console.log(mesObj)
+
+onHover("radio", (a) => {
+    add([
+        pos(a.pos),
+        sprite("score_radio"),
+        area(),
+        "radio_score"
+    ])
+})
--- README.md
+++ README.md
@@ -3,12 +3,12 @@ This is a game created in the context of a university programming class called "
 
 I own all of the visual and audio assets except for the font.
 
-The premise of the game is that you play as a political party aiming to pass or stop a bill from being inacted in Switzerland by propagandizing to influence voters' opinions. The game plays kind of like Reigns, as in you have a choice of 2 actions at a time that you can pick from. The game will work using 3 main variables.
+Vote Now! is a minimal strategy game. The premise of the game is that you play as a political party aiming to pass or stop a bill from being inacted in Switzerland by propagandizing to influence voters' opinions. The game plays kind of like Reigns, as in you have a choice of 2 actions at a time that you can pick from. The game will work using 3 main variables.
 
 * Money. This determines if you can buy campaign materials. It can be earned by asking for donations from voters or sponsors.
 * Opinion/votes. This determines how much in your favor the vote is going. From 0 (no voting intentions in your favor) to 100 (all voting intentions to your favor), it will be influenced by purchasing add-space, distributing flyers and organizing events.
 * Image/optics. This determines if your party's image is in good shape or not. Currently, only one event can tarnish your image: organizing a protest in front of the Federal Parliament.
 
 ## Developper journal
-### From 25.06 to 25.07 (about 15-20 hours)
-Most of this period has been dedicated to create most of the graphical elements using Aseprite to draw and GIMP to format the tilemap. All is not done yet, as I want to have a menu, as well as a visual presentation/overlay to make the game feel like a whole. Plans have also been made to vary the elements' lighting to add variety, though this will come a later stage if I have time. Basic tiling and mapping of the textures as animations has been done, and basic placement of the visual has developped.
+### From 25.06 to 25.07 (about 20 hours)
+Most of this period has been dedicated to create most of the graphical elements using Aseprite to draw and GIMP to format the tilemap. All is not done yet, as I want to have a menu, as well as a visual presentation/overlay to make the game feel like a whole. Plans have also been made to vary the elements' lighting to add variety, though this will come a later stage if I have time. Basic tiling and mapping of the textures as animations has been done, and basic placement of the visual for prototyping has developped.
