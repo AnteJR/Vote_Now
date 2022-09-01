83ad07318262b157a4fa755177bdbaeccb3a931f
Joël Rimaz <jo.ante2011@gmail.com>
2022-09-01 17:13:48 +0200

    #9 - AVS done

--- JS/gameVars.js
+++ JS/gameVars.js
@@ -71,7 +71,7 @@ let scenarios = [                                        // scenarios stores all
         "6.12.1925",
         "The bill is accepted! With your coordination, the campaign was successful, and there is now an insurance in place for handicaped people, veterans and the elderly! This will go on as becoming the main source for retirement benefits as well as the only source of income for disabled people. In 1925, the bill is accepted by 65.4% of the population, 6 months after a popular initiative with the same goals was rejected.",
         "The bill is rejected. As of yet, there is still to be a fund for disabled people, veterans and the elderly. Outside of charity, that is, though this will hardly allows people affected to get by. And maybe most disheartening of all: this is already the second time the population voted no on this matter.",
-        "Veterans, elderly and disabled insurance bill (1925)\n\nCurrently, war veterans and disabled people get by either through charity, or their relatives taking car of them. In the case of the elderly, their only real option is the latter.\n\nAs such, and after Worl War 1, the Swiss government proposes this bill to insure those people by paying them a (somewhat) living wage for their conditions.",
+        "Veterans, elderly and disabled insurance bill (1925)\n\nCurrently, war veterans and disabled people get by either through charity, or their relatives taking care of them. In the case of the elderly, their only real option is the latter.\n\nAs such, and after Worl War 1, the Swiss government proposes this bill to insure those people by paying them a (somewhat) living wage for their conditions.",
         65.4
     ],
     [
--- JS/main.js
+++ JS/main.js
@@ -907,6 +907,7 @@ loadSprite("ui_bottom", "UI_2.png");
 loadSprite("ui_end", "Background_end.png");
 loadSprite("BG_Mission_0", "train_vote_BG.png");
 loadSprite("BG_Mission_1", "insurance.png");
+loadSprite("BG_Mission_2", "AVS.png");
 
 loadSprite("continue", "Continue.png");          // Sprites for texts I use as buttons
 loadSprite("to_menu", "To_menu.png");
